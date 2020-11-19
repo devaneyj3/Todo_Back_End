c914a06fdc608f23b0445c6da27a7bc5deb4a1d8
Jordan <jordandevaney28@gmail.com>
2020-11-19 15:12:02 -0500

    return id when posting

--- api/TodoRoute.js
+++ api/TodoRoute.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     const data = await db("todos");
-    console.log(data);
     try {
         if (data) {
             res.status(200).send(data);
@@ -17,12 +16,8 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    const data = await db("todos").insert({
-        name: req.body.name,
-        created_at: req.body.created_at,
-        completed: req.body.completed,
-    });
-    console.log(req.body);
+    const data = await db("todos").insert(req.body);
+    req.body.id = data[0];
     if (data) {
         res.status(200).send(req.body);
     }
