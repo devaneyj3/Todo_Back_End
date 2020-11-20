baf78c39cfbff4d1673fcfcd710cd65ab8a2bd3c
Jordan <jordandevaney28@gmail.com>
2020-11-20 14:32:12 -0500

    add two more fields to the schema

--- data/migrations/20201109175203_todos.js
+++ data/migrations/20201109175203_todos.js
@@ -2,8 +2,10 @@ exports.up = function (knex) {
     return knex.schema.createTable("todos", (tbl) => {
         tbl.increments();
         tbl.text("name").unique().notNullable();
+        tbl.text("description");
         tbl.text("created_at");
         tbl.text("completed");
+        tbl.text("completed_at");
     });
 };
 
